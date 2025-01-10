229d6417b1934e9b7573a351527a0dabd0354065
enadol <enadol@yahoo.com>
2025-01-10 12:22:50 +0100

    modified:   docs/data/goals.csv.py

--- docs/data/goals.csv.py
+++ docs/data/goals.csv.py
@@ -15,7 +15,8 @@ async def main():
     async with aiohttp.ClientSession() as session:
         understat = Understat(session)
         #player = await understat.get_player_grouped_stats(8418)
-        #shots = await understat.get_player_shots(8418, {'season': "2022", 'player_assisted': 'Alphonso Davies'})
+        #shots = await understat.get_player_shots(8418, {'season': "2022", \
+        # 'player_assisted': 'Alphonso Davies'})
         shots = await understat.get_player_shots(8393, {'season': "2024"})
         #print(json.dumps(player))
         omar_string=json.dumps(shots, indent=4, ensure_ascii=False)
@@ -25,8 +26,7 @@ async def main():
         with codecs.open("./omarshots.json", "w", "utf-8") as jsonfile:
             jsonfile.write(omar_string)
         jsonfile.close()
-        
-    
+
 nest_asyncio.apply()
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
@@ -36,7 +36,8 @@ df=pd.read_json('./omarshots.json')
 df_result_grouped=df.groupby(['result']).count().sort_values(by=['id'], ascending=False)['id']
 df_result_grouped.to_csv('./docs/result.csv', encoding='ascii')
 
-df_assisted_grouped=df.groupby(['player_assisted']).count().sort_values(by=['id'], ascending=False)['id']
+df_assisted_grouped=df.groupby(['player_assisted']).count().sort_values(by=['id'],\
+ascending=False)['id']
 df_assisted_grouped.to_csv('./docs/assisted.csv')
 
 df_type_grouped=df.groupby(['shotType']).count().sort_values(by=['id'], ascending=False)['id']
